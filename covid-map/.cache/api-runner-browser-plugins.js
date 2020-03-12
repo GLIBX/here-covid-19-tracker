228e99a25b26e73d8c6901784e0a7c7a18012538
dezzyboy <dezzyboy@tradebit.ca>
2020-03-12 16:16:35 -0400

    delete cache

--- covid-map/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"tracking-coronavirus-COVID-19","short_name":"tracking-coronavirus-COVID-19","start_url":"/","background_color":"#FFFFFF","theme_color":"#00AFAA","display":"minimal-ui","icon":"src/images/glibx-cloud-logo@4x.png"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
